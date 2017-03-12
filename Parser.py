af5381f5eb89d1325098abfd55045a0772d0922a
Nick Westin <nicolaswestin@gmail.com>
2017-03-11 19:14:10 -0500

    Update character parsing

--- Parser.py
+++ Parser.py
@@ -1,4 +1,3 @@
-import urllib
 from lxml import html
 import requests
 
@@ -20,6 +19,7 @@ class MarvelParser:
         """
         self.year_to_start = year_to_start
         self.all_comics_url = all_comics_url
+        self.base_url = self.all_comics_url[:self.all_comics_url.rfind("/")]
 
     # issueName, issueNum, seriesName, releaseDate, issueID, seriesID, imageURL
     def save_issue_data(self, issue_url):
@@ -28,7 +28,7 @@ class MarvelParser:
 
             example:
 
-            issueName,issueNum,seriesName,releaseDate,image,issueID,seriesID
+            issue_name,issue_num,series_name,release_date,image,issueID,seriesID
             The Hunger: Part 1 of 5,1,The Spectacular Spider-Man Vol 2,"September, 2003",1,1
             The Hunger: Part 2 of 5,2,The Spectacular Spider-Man Vol 2,"September, 2003",2,1
         """
@@ -37,72 +37,110 @@ class MarvelParser:
         tree = html.fromstring(page.content)
 
         # Issue Name
-        issueName = tree.xpath(
+        issue_name = tree.xpath(
             '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/table/tr[1]/th/div/b/a/text()')
-        if len(issueName) > 1:
-            issueName = issueName[0].strip() + ' & ' + issueName[1].strip()
-        elif len(issueName) == 1:
-            issueName = issueName[0]
-        elif len(issueName) == 0:
-            issueName = ""
+        if len(issue_name) > 1:
+            issue_name = issue_name[0].strip() + ' & ' + issue_name[1].strip()
+        elif len(issue_name) == 1:
+            issue_name = issue_name[0]
+        elif len(issue_name) == 0:
+            issue_name = ""
 
         # Series Name
-        seriesName = tree.xpath(
+        series_name = tree.xpath(
             '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[1]/a/text()')
-        if len(seriesName) == 0:
-            seriesName = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/a/text()')[
+        if len(series_name) == 0:
+            series_name = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/a/text()')[
                 0].strip()
         else:
-            seriesName = seriesName[0].strip()
-        seriesName = seriesName.replace('  ', ' ')
+            series_name = series_name[0].strip()
+        series_name = series_name.replace('  ', ' ')
 
         # Image URL
-        imageURL = tree.xpath(
+        image_url = tree.xpath(
             '//*[@id="templateimage"]/div/div/a/img/@src')[0]
-        imageURL = imageURL[:imageURL.find('.jpg') + 4].strip()
+        image_url = image_url[:image_url.find('.jpg') + 4].strip()
 
         # Issue Number
-        issueNum = tree.xpath(
+        issue_num = tree.xpath(
             '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[1]/text()')
-        if len(issueNum) == 0:
-            issueNum = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/text()')
-        issueNum = issueNum[0].strip()
-        issueNum = issueNum[issueNum.find('#') + 1:].strip()
+        if len(issue_num) == 0:
+            issue_num = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/text()')
+        issue_num = issue_num[0].strip()
+        issue_num = issue_num[issue_num.find('#') + 1:].strip()
 
         # Release Date
-        releaseDateXpaths = ['//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/a/text()',
-                             '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()',
-                             '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/text()',
-                             '//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()']
-        releaseDate = [tree.xpath(currentXpath) for currentXpath in releaseDateXpaths if tree.xpath(currentXpath)]
-        if len(releaseDate[0]) > 1:
-            releaseDate = releaseDate[0][0] + ', ' + releaseDate[0][1]
+        release_date_xpaths = ['//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/a/text()',
+                               '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()',
+                               '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/text()',
+                               '//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()']
+        release_date = [tree.xpath(currentXpath) for currentXpath in release_date_xpaths if tree.xpath(currentXpath)]
+        if len(release_date[0]) > 1:
+            release_date = release_date[0][0] + ', ' + release_date[0][1]
         else:
-            releaseDate = releaseDate[0][0]
+            release_date = release_date[0][0]
 
-        print(releaseDate)
-        entry = [issueName.replace('\"', ''), issueNum.replace('\"', ''), seriesName.replace('\"', ''), releaseDate,
-                 imageURL]
+        print(release_date)
+        entry = [issue_name.replace('\"', ''), issue_num.replace('\"', ''), series_name.replace('\"', ''), release_date,
+                 image_url]
         print(entry)
+
+    def save_character_data(self, character_url, index):
+        page = requests.get(self.base_url + character_url)
+        tree = html.fromstring(page.content)
+        # Character Name
+        character_name = tree.xpath(
+            '')
+        # Earth ID
+        earth_id = tree.xpath(
+            '')
+        # Homeworld
+        homeworld = tree.xpath(
+            '')
+        # Affiliation
+        affiliation = tree.xpath(
+            '')
+        # Type of Character
+        type_of_char = ""
+        if index == 1:
+            type_of_char = "Protagonist"
+        elif index == 2:
+            type_of_char = "Supporting"
+        elif index == 3:
+            type_of_char = "Antagonist"
+
+    # list of character url
+    def parse_characters_data(self, list_of_characters, index):
+        [self.save_character_data(character_url, index) for character_url in list_of_characters]
 
     # write to character.csv
     # charName, typeOfChar, earthID, issueID, charID
-    def save_character_data(self, issue_url):
+    def parse_characters_url(self, issue_url):
         """
             Write a character data as a csv file
             example:
 
-            "Name","Race","Homeworld","Affiliation"
-            "C-3 PO","Droid","Unknown","rebels"
-            "Chewbacca","Wookie","Kashyyyk","rebels"
-            "Darth Vader","Human","Unknown","empire"
-            "Han Solo","Human","Corellia","rebels"
+            charName, typeOfChar, earthID, issueID, charID
         """
         # For each parameter,
         #    save it in the proper column
         print(issue_url)
         page = requests.get(issue_url)
         tree = html.fromstring(page.content)
+
+        featured_character_url = tree.xpath(
+            '//div[@id="mw-content-text"]/h2[@id="AppearingHeader1"]/' +
+            'following::p[b[contains(text(), "Featured Characters")]]/following::ul[1]//a/@href')
+        supporting_character_url = tree.xpath(
+            '//div[@id="mw-content-text"]/h2[@id="AppearingHeader1"]' +
+            '/following::p[b[contains(text(), "Supporting Characters")]]/following::ul[1]//a/@href')
+        antagonist_character_url = tree.xpath(
+            '//div[@id="mw-content-text"]/h2[@id="AppearingHeader1"]' +
+            '/following::p[b[contains(text(), "Antagonists")]]/following::ul[1]//a/@href')
+        all_types_of_characters = [featured_character_url, supporting_character_url, antagonist_character_url]
+        #                         (list_of_url  index)     [list_of_url, list_of_url, list_of_url]
+        [self.parse_characters_data(characters, index) for index, characters in
+         enumerate(all_types_of_characters) if len(characters) > 0]
 
     def parse_series_data(self):
         """
@@ -114,7 +152,6 @@ class MarvelParser:
         #   save it as a list
         #   pass all data in that issues page to save_series_data funtcion
         valid_series_urls = self.grab_series_url()
-
         for series_url in valid_series_urls:
             page = requests.get(series_url)
             tree = html.fromstring(page.content)
@@ -122,7 +159,7 @@ class MarvelParser:
             issue_url_endings = tree.xpath('//*[contains(@id,"gallery")]/div[*]/div[*]/div/b/a/@href')
             issue_urls = [home_url + issue for issue in issue_url_endings]
             [self.save_issue_data(issue_url) for issue_url in issue_urls]
-            # [self.save_character_data(issue_url) for issue_url in issue_urls]
+            [self.parse_characters_url(issue_url) for issue_url in issue_urls]
 
     def parse(self):
         self.parse_series_data()
