496456b35c82ee4c1754f58dd81d0dc9aadc1b4a
Nick Westin <nicolaswestin@gmail.com>
2017-03-28 21:39:09 -0400

    Finished data parser implementation. CSV saving is required.

--- Parser.py
+++ Parser.py
@@ -1,5 +1,7 @@
-from lxml import html
+import csv
+
 import requests
+from lxml import html
 
 
 class MarvelParser:
@@ -10,7 +12,9 @@ class MarvelParser:
                         Select only series that start after yearToStart.
     """
 
-    def __init__(self, year_to_start=0, all_comics_url='http://marvel.wikia.com/wiki/Spider-Man_Comic_Books'):
+    def __init__(self,
+                 year_to_start=0,
+                 all_comics_url='http://marvel.wikia.com/wiki/Spider-Man_Comic_Books'):
         """
             Construct parser that grabs all comics found at all_comics_url that start on or after year_to_start.
 
@@ -19,18 +23,34 @@ class MarvelParser:
         """
         self.year_to_start = year_to_start
         self.all_comics_url = all_comics_url
-        self.base_url = self.all_comics_url[:self.all_comics_url.rfind("/")]
+        self.base_url = self.all_comics_url[:self.all_comics_url.rfind("/wiki/")]
+        self.characters = []
+        self.issues = []
+
+    def csv_save(self, data):
+        print("not done yet")
+        # if len(data) == 4:
+        #    with open("character.csv", 'w') as resultFile:
+        #        wr = csv.writer(resultFile, dialect='excel')
+        #        wr.writerows(data)
+        # elif len(data) == 5:
+        #    with open("issue.csv", 'w') as resultFile:
+        #        wr = csv.writer(resultFile, dialect='excel')
+        #        wr.writerows(data)")
+
+    def data_save(self, data):
+        if len(data) == 4:
+            self.characters.append(data)
+        elif len(data) == 5:
+            self.issues.append(data)
 
     # issueName, issueNum, seriesName, releaseDate, issueID, seriesID, imageURL
     def save_issue_data(self, issue_url):
         """
             Write an issues data as a csv file
-
-            example:
-
-            issue_name,issue_num,series_name,release_date,image,issueID,seriesID
-            The Hunger: Part 1 of 5,1,The Spectacular Spider-Man Vol 2,"September, 2003",1,1
-            The Hunger: Part 2 of 5,2,The Spectacular Spider-Man Vol 2,"September, 2003",2,1
+    
+            format:
+            issue_name,issue_num,series_name,release_date,image
         """
         print(issue_url)
         page = requests.get(issue_url)
@@ -50,8 +70,10 @@ class MarvelParser:
         series_name = tree.xpath(
             '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[1]/a/text()')
         if len(series_name) == 0:
-            series_name = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/a/text()')[
-                0].strip()
+            series_name = \
+                tree.xpath(
+                    '//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/a/text()')[
+                    0].strip()
         else:
             series_name = series_name[0].strip()
         series_name = series_name.replace('  ', ' ')
@@ -65,38 +87,43 @@ class MarvelParser:
         issue_num = tree.xpath(
             '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[1]/text()')
         if len(issue_num) == 0:
-            issue_num = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/text()')
+            issue_num = tree.xpath(
+                '//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[1]/text()')
         issue_num = issue_num[0].strip()
         issue_num = issue_num[issue_num.find('#') + 1:].strip()
 
         # Release Date
-        release_date_xpaths = ['//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/a/text()',
-                               '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()',
-                               '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/text()',
-                               '//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()']
-        release_date = [tree.xpath(currentXpath) for currentXpath in release_date_xpaths if tree.xpath(currentXpath)]
+        release_date_xpaths = [
+            '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/a/text()',
+            '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()',
+            '//*[@id="mw-content-text"]/table[1]/tr[1]/td/div/div[2]/div[2]/div[4]/text()',
+            '//*[@id="mw-content-text"]/table[2]/tr[1]/td/div/div[2]/div[2]/div[4]/a/text()']
+        release_date = [tree.xpath(currentXpath) for currentXpath in release_date_xpaths if
+                        tree.xpath(currentXpath)]
         if len(release_date[0]) > 1:
             release_date = release_date[0][0] + ', ' + release_date[0][1]
         else:
             release_date = release_date[0][0]
+        issue_data = [issue_name.replace('\"', ''),
+                      issue_num.replace('\"', ''),
+                      series_name.replace('\"', ''),
+                      release_date,
+                      image_url]
+        self.data_save(issue_data)
+        print(issue_data)
 
-        print(release_date)
-        entry = [issue_name.replace('\"', ''), issue_num.replace('\"', ''), series_name.replace('\"', ''), release_date,
-                 image_url]
-        print(entry)
-
-    def save_character_data(self, character_url, index):
-        page = requests.get(self.base_url + character_url)
+    def save_character_data(self, character_url, index, issueID):
+        url = self.base_url + character_url
+        page = requests.get(url)
         tree = html.fromstring(page.content)
         # Character Name
         character_name = str(tree.xpath(
             '//*[@id="WikiaPageHeader"]/div/div[1]/h1/text()')[0])
-        begin = character_name.find('Wiki/') + 5
         end = character_name.rfind(' (Earth')
         if end < 0:
-            character_name = character_name[begin:]
+            character_name = character_name[:]
         else:
-            character_name = character_name[begin:end]
+            character_name = character_name[:end]
         # Type of Character
         type_of_char = ""
         if index == 0:
@@ -107,21 +134,34 @@ class MarvelParser:
             type_of_char = "Antagonist"
         # Earth ID
         earth_id = tree.xpath(
-            '//*[@id="mw-content-text"]/div[1]/aside/section[3]/div[2]/div/a/text()')
-        print(character_name)
-        print([character_name, type_of_char, earth_id])
+            '//*[@id="mw-content-text"]/div[1]//' +
+            'h3[contains(text(),"Universe")]/following-sibling::*/a/text()')
+        if (len(earth_id) > 0):
+            earth_id = str(earth_id[0])
+        else:
+            eid_ind = character_url.find('(Earth-')
+            if eid_ind >= 0:
+                earth_id = character_url[eid_ind + 1:-1]
+            else:
+                earth_id = ''
+        character_data = [character_name, type_of_char, earth_id, str(issueID)]
+        # print(character_data[0] + " : " + character_url)
+        # print(character_data)
+        self.data_save(character_data)
+        print(character_data)
 
     # list of character url
-    def parse_characters_data(self, list_of_characters, index):
-        [self.save_character_data(character_url, index) for character_url in list_of_characters]
+    def parse_characters_data(self, list_of_characters, index, issueID):
+        [self.save_character_data(character_url, index, issueID) for character_url in
+         list_of_characters]
 
     # write to character.csv
     # charName, typeOfChar, earthID, issueID, charID
-    def parse_characters_url(self, issue_url):
+    def parse_characters_url(self, issueID, issue_url):
         """
             Write a character data as a csv file
             example:
-
+    
             charName, typeOfChar, earthID, issueID, charID
         """
         # For each parameter,
@@ -145,9 +185,10 @@ class MarvelParser:
         # Make a list of all character url's and create a set of them to remove reoccurences
         # if there are two stories in an issue
         all_types_of_characters = [list(set(url_list)) for url_list in
-                                   [featured_character_url, supporting_character_url, antagonist_character_url]]
+                                   [featured_character_url, supporting_character_url,
+                                    antagonist_character_url]]
         #                         (list_of_url  index)     [list_of_url, list_of_url, list_of_url]
-        [self.parse_characters_data(characters, index) for index, characters in
+        [self.parse_characters_data(characters, char_type, issueID) for char_type, characters in
          enumerate(all_types_of_characters) if len(characters) > 0]
 
     def parse_series_data(self):
@@ -164,10 +205,12 @@ class MarvelParser:
             page = requests.get(series_url)
             tree = html.fromstring(page.content)
             home_url = 'http://marvel.wikia.com'
-            issue_url_endings = tree.xpath('//*[contains(@id,"gallery")]/div[*]/div[*]/div/b/a/@href')
+            issue_url_endings = tree.xpath(
+                '//*[contains(@id,"gallery")]/div[*]/div[*]/div/b/a/@href')
             issue_urls = [home_url + issue for issue in issue_url_endings]
-            [self.save_issue_data(issue_url) for issue_url in issue_urls]
-            # [self.parse_characters_url(issue_url) for issue_url in issue_urls]
+            # [self.save_issue_data(issue_url) for issue_url in issue_urls]
+            [self.parse_characters_url(issueID, issue_url) for issueID, issue_url in
+             enumerate(issue_urls, 1)]
 
     def parse(self):
         self.parse_series_data()
@@ -175,7 +218,7 @@ class MarvelParser:
     def grab_series_url(self):
         """
             Returns a list of URLs (list of comic book series) to look in and grab data from.
-
+    
             Grabs the series that start after this MarvelParsers yearToStart year.
         """
         # For every comic visible at the given link
@@ -185,11 +228,12 @@ class MarvelParser:
         page = requests.get(self.all_comics_url)
         tree = html.fromstring(page.content)
 
-        home_url = 'http://marvel.wikia.com'
-        series_start_dates = tree.xpath('//*[@id="gallery-0"]/div[*]/div[2]/center/i/a[1]/text()')
+        series_start_dates = tree.xpath(
+            '//*[@id="gallery-0"]/div[*]/div[2]/center/i/a[1]/text()')
         series_start_dates = list(map(int, series_start_dates))
-        series_urls = tree.xpath('//*[@id="gallery-0"]/div[*]/div[*]/center[i/a[1]/@href]/b/a/@href')
-        valid_series_urls = [home_url + comic for index, comic in enumerate(series_urls)
+        series_urls = tree.xpath(
+            '//*[@id="gallery-0"]/div[*]/div[*]/center[i/a[1]/@href]/b/a/@href')
+        valid_series_urls = [self.base_url + comic for index, comic in enumerate(series_urls)
                              if series_start_dates[index] >= self.year_to_start]
         return valid_series_urls
 
@@ -199,6 +243,6 @@ class MarvelParser:
 # =====================================================================================================================
 
 if __name__ == "__main__":
-    spiderman = MarvelParser()
+    spiderman = MarvelParser(2014)
     print("All+:\n")
     spiderman.parse()
